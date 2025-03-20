5ff02c5f86ca446215d56dcb06d68429591691ad
Thasika Ragupathy <thasikathasi01@gmail.com>
2025-03-20 22:38:42 +0530

    Add files via upload
    Basic function to print message and calculation

---
+++ program/Functions.js
@@ -0,0 +1,20 @@
+//Functions to print message
+function PrintMsg()
+{
+	console.log("hello JS")
+}
+PrintMsg()
+
+//function to Calculate sum
+function sum()
+{
+	return 5+6
+}
+console.log(sum())
+
+//function to Calculate substraction
+function sub(a,b)
+{
+	return a-b
+}
+console.log(sub(5,3))
