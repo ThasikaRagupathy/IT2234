168f3e981b9e97f881502dc63614cd2435fd64d3
Thasika Ragupathy <thasikathasi01@gmail.com>
2025-04-07 23:09:34 +0530

    Add files via upload

---
+++ Code/myApp2/DB/studentdb.js
@@ -0,0 +1,8 @@
+let students=[
+    {regno:'2021ict01',name:'James',age:21,course:'IT'},
+    {regno:'2021ict02',name:'John',age:23,course:'AMC'},
+    {regno:'2021ict03',name:'Smith',age:22,course:'BIO'},
+    {regno:'2021ict04',name:'Zara',age:26,course:'AMC'},
+    {regno:'2021ict05',name:'William',age:24,course:'IT'}]
+
+    module.exports=students;
